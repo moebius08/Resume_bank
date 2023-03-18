83b851bcdf875e3972c50d3915ed1e5eb83dbd1e
mavcuevas <mavcuevas@iskolarngbayan.pup.edu.ph>
2023-03-18 19:12:59 +0800

    Update about_us_content.php

--- page/about_us_content.php
+++ page/about_us_content.php
@@ -30,7 +30,7 @@
             <h2>Matt Gabriel Camaing</h2>
             <p class="title">Designer</p>
             <p>.</p>
-            <p>matt.camaing@gmail.com.com</p>
+            <p>matt.camaing@gmail.com</p>
             <p><button class="button">Contact</button></p>
           <div class="d-flex justify-content-center mb-2">
           </div>
@@ -46,7 +46,7 @@
             <h2>Moris Assanty Cuevas</h2>
             <p class="title">Developer</p>
             <p>.</p>
-            <p>morisassantycuevas@gmail.com</p>
+            <p>moriscuevas3@gmail.com</p>
             <p><button class="button">Contact</button></p>
           <div class="d-flex justify-content-center mb-2">
           </div>
@@ -58,7 +58,7 @@
       <div class="card my-5 w-100">
         <div class="card-body text-center">
           <img src="<?=$action->helper->loadimage('krgorod.jpg')?>" alt="avatar"
-            class="rounded-circle img-fluid" style="width: 200px;">
+            class="rounded-circle img-fluid" style="width: 167px;">
             <h2>Keno Rei Dorog</h2>
             <p class="title">Designer</p>
             <p>.</p>
